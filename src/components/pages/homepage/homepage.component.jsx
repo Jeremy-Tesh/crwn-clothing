f197075b05f3e814ff752c95cf19e83ae780e220
Jeremy-Tesh <jermitesh20@gmail.com>
2021-04-13 00:00:10 +0300

    created project and styled components

---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../directory/directory.component";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+export default Homepage;
