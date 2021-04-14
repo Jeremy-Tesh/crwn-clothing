1e0384587913d22c143d6e5952ebf8977fa02220
Jeremy-Tesh <jermitesh20@gmail.com>
2021-04-14 17:23:57 +0300

    update our menu-items withRouter Hoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 import Homepage from "./components/pages/homepage/homepage.component";
+
+const Hats = () => {
+  return <div>hats page</div>;
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route path="/hats" component={Hats} />
     </div>
   );
 }
